8acf75f071b5d12aa99f35100a83ee000e505f28
FMR7 <14892931+FMR7@users.noreply.github.com>
2025-04-25 19:07:04 +0200

    host

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ app.post('/api/contacto', (req, res) => {
 });
 
 const PORT = 80;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
